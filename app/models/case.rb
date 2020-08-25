a99af7786b16b05551d2db2155e17c71648ea00c
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-25 08:32:15 -0400

    created route for #home#index page

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,2 +1,9 @@
-class Case < ApplicationRecord
+class Case < ActiveRecord::Base
+  attr_accessible :casenum, :name, :insurance, :treatmentplan
+
+  validates :casenum, presence => true
+  validates :name, :presence => true,
+  # validates :insurance, :presence => true,
+  # validates :treatmentplan, :presence => true
+
 end
