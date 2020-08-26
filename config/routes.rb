14d6a94d162ee00e5300bb68ef84acab3f20e78f
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-26 11:45:52 -0400

    draw RESTful routes for cases_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
   #get 'home/index'
-  root :to => "home#index"
+  root "static#home"
+
+  post '/cases', to: 'cases#create'
+  get '/cases/new', to: 'cases#new', as: 'new_case'
+  get '/cases/:id', to: 'cases#show', as: 'case'
+  get '/cases/:id/edit', to: 'cases#edit', as: 'edit_case'
+  patch '/cases/:id', to: 'cases#update'
+  post '/patients', to: 'patients#create', as: 'patients'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
