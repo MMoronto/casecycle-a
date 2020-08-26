8922cc21dfbb91f06145cee6bfc1622ba50d9a3b
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-26 12:00:31 -0400

    draw RESTful route for index method in cases_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   #get 'home/index'
   root "static#home"
-
+  get '/cases', to: 'cases#index', as: 'cases'
   post '/cases', to: 'cases#create'
   get '/cases/new', to: 'cases#new', as: 'new_case'
   get '/cases/:id', to: 'cases#show', as: 'case'
