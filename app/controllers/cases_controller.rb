ec8fb08c3f3c685adabf71fa7c7f887daf6d35f8
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-26 10:03:37 -0400

    add index and create methods to CasesController class

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,2 +1,26 @@
 class CasesController < ApplicationController
+  def index
+    @cases = Case.all
+  end
+
+  def create
+    case = Case.create(case_params)
+    redirect_to case_path(case)
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
