ade023af36069ca5a61df372aa4277d1c3ac277b
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-30 05:14:55 -0400

    add create method action and strong params to patient_controller class

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,16 @@
 class PatientsController < ApplicationController
+  def create
+    patient = patient.create(patient_params)
+    message = patient.take_patient
+    redirect_to doctor_path(patient.doctor), flash: { message: message }
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(
+    :doctor_id,
+    :attraction_id
+    )
+  end
 end
