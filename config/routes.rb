bd5a3abbb6d73d8d32b601b59d40df3a28c56880
M Moronto <T2WYCEMO@GMAIL.com>
2020-08-31 14:20:14 -0400

    Revise routes to match revised controller methods and erb pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get '/doctors/new', to: 'doctors#new', as: 'new_doctor'
   root "static#home"
   get '/doctors', to: 'doctors#index', as: 'doctors'
-    get '/cases', to: 'cases#index', as: 'cases'
-  post '/cases', to: 'cases#create'
-  get '/cases/new', to: 'cases#new', as: 'new_case'
-  get '/cases/:id', to: 'cases#show', as: 'case'
-  get '/cases/:id/edit', to: 'cases#edit', as: 'edit_case'
-  patch '/cases/:id', to: 'cases#update'
+    get '/treatments', to: 'treatments#index', as: 'treatments'
+  post '/treatments', to: 'treatments#create'
+  get '/treatments/new', to: 'treatments#new', as: 'new_treatment'
+  get '/treatments/:id', to: 'treatments#show', as: 'treatment'
+  get '/treatments/:id/edit', to: 'treatments#edit', as: 'edit_treatment'
+  patch '/treatments/:id', to: 'treatments#update'
   post '/patients', to: 'patients#create', as: 'patients'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
