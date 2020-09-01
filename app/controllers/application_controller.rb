17c9e1af60f23cd3cb44a28b0031a23092eb5aa8
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-01 16:41:43 -0400

    add authentication and verification action methods in application_controller class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :verified_doctor
+  helper_method :current_doctor
+
+  private
+
+  def verified_doctor
+    redirect_to '/' unless doctor_is_authenticated
+  end
+
+  def doctor_is_authenticated
+    !!current_doctor
+  end
+
+  def current_doctor
+    Doctor.find_by(id: session[:doctor_id])
+  end
 end
