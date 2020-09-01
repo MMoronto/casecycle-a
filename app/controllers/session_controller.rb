cc61e5a7e1c38a8ff9d786edb693277040354c34
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-01 16:29:28 -0400

    write sessions controller to handle treatment#new/create routes

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,21 @@
+class SessionController < ApplicationController
+  skip_before_action :verified_doctor, only: [:new, :create]
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    if @doctor = Doctor.find_by(name: params[:doctor][:name])
+      session[:doctor_id] = @doctor.id
+      redirect_to user_path(@doctor)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete("doctor_id")
+    redirect_to root_path
+  end
+end
