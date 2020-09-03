a0d564e42d316f7fb992d6953ccf5b59957bb0fc
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-03 05:46:56 -0400

    add get routes for doctors#show & #edit controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-
   get '/doctors/new', to: 'doctors#new', as: 'new_doctor'
+
   root "static#home"
   get '/doctors', to: 'doctors#index', as: 'doctors'
-    get '/treatments', to: 'treatments#index', as: 'treatments'
+  post '/doctors', to:'doctors#create'
+  get '/doctors/:id', to: 'doctors#show', as: 'doctor'
+  get '/doctors/:id/edit', to: 'doctors#edit', as: 'edit_doctor'
+  get '/treatments', to: 'treatments#index', as: 'treatments'
   post '/treatments', to: 'treatments#create'
   get '/treatments/new', to: 'treatments#new', as: 'new_treatment'
   get '/treatments/:id', to: 'treatments#show', as: 'treatment'
