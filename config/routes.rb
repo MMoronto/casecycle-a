9965ddddf853f61c946390faa0c1c21977d20960
M Moronto <T2WYCEMO@GMAIL.com>
2020-09-03 06:00:20 -0400

    add delete route for session#destroy controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/doctors/new', to: 'doctors#new', as: 'new_doctor'
-
   root "static#home"
   get '/doctors', to: 'doctors#index', as: 'doctors'
   post '/doctors', to:'doctors#create'
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
   get '/treatments', to: 'treatments#index', as: 'treatments'
   get '/signin', to: 'session#new', as: 'signin'
   post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy'
   post '/treatments', to: 'treatments#create'
   get '/treatments/new', to: 'treatments#new', as: 'new_treatment'
   get '/treatments/:id', to: 'treatments#show', as: 'treatment'
